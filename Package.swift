bec7cc6ffc9f2131f0ddd7d82c771af01bcdffa6
IvanStepanok <ivan.stepanok@raccoongang.com>
2024-10-09 17:01:51 +0300

    fix: remove swiftGen

--- Package.swift
+++ Package.swift
@@ -19,8 +19,7 @@ let package = Package(
         .package(url: "https://github.com/Alamofire/Alamofire.git", exact: "5.8.0"),
         .package(url: "https://github.com/onevcat/Kingfisher.git", exact: "7.9.1"),
         .package(url: "https://github.com/siteline/swiftui-introspect", from: "1.3.0"),
-        .package(url: "https://github.com/Swinject/Swinject.git", from: "2.9.1"),
-        .package(url: "https://github.com/SwiftGen/SwiftGenPlugin", exact: "6.6.0")
+        .package(url: "https://github.com/Swinject/Swinject.git", from: "2.9.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -32,9 +31,6 @@ let package = Package(
                 .product(name: "Kingfisher", package: "Kingfisher"),
                 .product(name: "SwiftUIIntrospect", package: "swiftui-introspect"),
                 .product(name: "Swinject", package: "Swinject")
-            ],
-            plugins: [
-                .plugin(name: "SwiftGenPlugin", package: "SwiftGenPlugin")
             ]
         ),
         .testTarget(

660200cd3e525c9c3c9f6cb476afa4ba72f9aa2b
IvanStepanok <ivan.stepanok@raccoongang.com>
2024-10-17 16:50:32 +0300

    fix: change type to dynamic

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "OEXFoundation",
+            type: .dynamic,
             targets: ["OEXFoundation"]),
     ],
     dependencies: [

585f3677815116a7e3d45a6e74ec4c4003b57291
Volodymyr Chekyrta <volodymyr.chekyrta@raccoongang.com>
2024-10-24 18:40:50 +0300

    build: Setup ci (#5)
    * chore: delete unused data
    
    * build: setup GitHub actions

---
+++ Tests/OEXFoundationTests/SampleTest.swift
@@ -0,0 +1,15 @@
+//
+//  SampleTest.swift
+//  OEXFoundation
+//
+//  Created by Volodymyr Chekyrta on 24.10.24.
+//
+
+import Testing
+import Foundation
+@testable import OEXFoundation
+
+@Test
+func sampleTest() async throws {
+    assert(2 + 2 == 4)
+}

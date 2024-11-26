95b9f80045df76379da51d1b78b97a56841b59df
Anton Yarmolenko <37253+rnr@users.noreply.github.com>
2024-11-26 15:04:30 +0100

    chore: move DeepLinkService protocol back

--- Sources/OEXFoundation/DeepLinkManager/DeepLinkManager.swift
+++ Sources/OEXFoundation/DeepLinkManager/DeepLinkManager.swift
@@ -8,20 +8,6 @@
 import Foundation
 import UIKit
 
-public protocol DeepLinkService {
-    func configureWith(
-        manager: DeepLinkManagerProtocol,
-        config: ConfigProtocol, // ToDo: move ConfigProtocol to OEXFoundation
-        launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    )
-    
-    func handledURLWith(
-        app: UIApplication,
-        open url: URL,
-        options: [UIApplication.OpenURLOptionsKey: Any]
-    ) -> Bool
-}
-
 public protocol DeepLinkManagerProtocol {
     func processLinkFrom(userInfo: [AnyHashable: Any])
 }

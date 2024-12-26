fc27a5fad71e95e8e7a9bd24434ec39887bf3874
Anton Yarmolenko <37253+rnr@users.noreply.github.com>
2024-12-26 16:03:57 +0100

    Revert "chore: removed functions from protocol"
    This reverts commit fa15242deb44b9834e3eeb888361977e7cf16268.

--- Sources/OEXFoundation/DeepLinkManager/DeepLinkManager.swift
+++ Sources/OEXFoundation/DeepLinkManager/DeepLinkManager.swift
@@ -8,4 +8,7 @@
 import Foundation
 import UIKit
 
-public protocol DeepLinkManagerProtocol: Sendable {}
+public protocol DeepLinkManagerProtocol: Sendable {
+    func processLinkFrom(userInfo: [AnyHashable: Any])
+    func processDeepLink(with params: [AnyHashable: Any]?)
+}

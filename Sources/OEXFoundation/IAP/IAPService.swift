d21d1fd81d44b13b080ab16228d93761533da26c
Anton Yarmolenko <37253+rnr@users.noreply.github.com>
2025-01-28 17:50:15 +0100

    chore: iap protocolo uiview representable

--- Sources/OEXFoundation/IAP/IAPService.swift
+++ Sources/OEXFoundation/IAP/IAPService.swift
@@ -7,14 +7,29 @@
 
 import SwiftUI
 
+public struct ViewRepresentable: UIViewRepresentable {
+    var uiView: UIView
+    
+    public init(uiView: UIView) {
+        self.uiView = uiView
+    }
+    
+    public func makeUIView(context: Context) -> UIView {
+        uiView
+    }
+    
+    public func updateUIView(_ uiView: UIView, context: Context) {}
+}
+
+@MainActor
 public protocol IAPServiceProtocol {
-    associatedtype ContentView: View
-    func someTestView() -> ContentView
+    func someTestView() -> UIView
+//    @ViewBuilder @MainActor @preconcurrency var body: Self.ContentView { get }
 }
 
 public struct IAPCommonService: IAPServiceProtocol {
     public init() {}
-    public func someTestView() -> some View {
-        EmptyView()
+    public func someTestView() -> UIView {
+        UIHostingController(rootView: EmptyView()).view
     }
 }

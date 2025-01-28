c3b2b83dc39afc7972cd567fe655c00a3f400f3c
Anton Yarmolenko <37253+rnr@users.noreply.github.com>
2025-01-28 13:48:43 +0100

    chore: add IAP service

---
+++ Sources/OEXFoundation/IAP/IAPService.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  OEXFoundation
+//
+//  Created by Anton Yarmolenka on 28/01/2025.
+//
+
+import SwiftUI
+
+public protocol IAPServiceProtocol {
+    func someTestView() -> any View
+}

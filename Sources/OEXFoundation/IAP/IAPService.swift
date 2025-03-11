fb38f90f94cf76493858f05b0bbb3c0d8994ec09
Vadim Kuznetsov <vadim@touchappmedia.com>
2025-03-11 19:13:34 +0300

    chore: changed protocol

--- Sources/OEXFoundation/IAP/IAPService.swift
+++ Sources/OEXFoundation/IAP/IAPService.swift
@@ -7,34 +7,15 @@
 
 import SwiftUI
 
-// ------- V1 ---------
-public struct ViewRepresentable: UIViewRepresentable {
-    var uiView: UIView
-    
-    public init(uiView: UIView) {
-        self.uiView = uiView
-    }
-    
-    public func makeUIView(context: Context) -> UIView {
-        uiView
-    }
-    
-    public func updateUIView(_ uiView: UIView, context: Context) {}
+public protocol IAPConfiguration {
+    var productName: String { get }
+    var sku: String { get }
+    var courseID: String { get }
 }
 
 @MainActor
 public protocol IAPServiceProtocol {
-    func someTestView() -> UIView?
-}
-
-public struct IAPCommonService: IAPServiceProtocol {
-    public init() {}
-    public func someTestView() -> UIView? {
-        nil
-    }
-}
-
-// ------- V2 ---------
-public protocol V2IAPServiceProtocol {
-    func someTestView() -> AnyView
+    associatedtype Configuration: IAPConfiguration
+    func dashboardPrimaryCardButton(configuration: Configuration) -> AnyView
+    func courseButton(configuration: Configuration) -> AnyView
 }

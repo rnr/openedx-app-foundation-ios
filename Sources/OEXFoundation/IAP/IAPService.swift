d0f07c3bed7672dd92cb37f84b843b5221083188
Vadim Kuznetsov <vadim@touchappmedia.com>
2025-03-11 23:28:09 +0300

    chore: added AnyIAPService

--- Sources/OEXFoundation/IAP/IAPService.swift
+++ Sources/OEXFoundation/IAP/IAPService.swift
@@ -19,3 +19,41 @@ public protocol IAPServiceProtocol {
     func dashboardPrimaryCardButton(configuration: Configuration) -> AnyView
     func courseButton(configuration: Configuration) -> AnyView
 }
+
+@MainActor
+public protocol AnyIAPServiceProtocol {
+    init<Service: IAPServiceProtocol>(_ service: Service)
+    
+    func dashboardPrimaryCardButton(configuration: Any) -> AnyView?
+    func courseButton(configuration: Any) -> AnyView?
+}
+
+@MainActor
+public struct AnyIAPService: AnyIAPServiceProtocol {
+    private let _dashboardPrimaryCardButton: (Any) -> AnyView?
+    private let _courseButton: (Any) -> AnyView?
+
+    public init<Service: IAPServiceProtocol>(_ service: Service) {
+        _dashboardPrimaryCardButton = { config in
+            guard let config = config as? Service.Configuration else {
+                return nil
+            }
+            return service.dashboardPrimaryCardButton(configuration: config)
+        }
+        
+        _courseButton = {@MainActor config in
+            guard let config = config as? Service.Configuration else {
+                return nil
+            }
+            return service.courseButton(configuration: config)
+        }
+    }
+    
+    public func dashboardPrimaryCardButton(configuration: Any) -> AnyView? {
+        return _dashboardPrimaryCardButton(configuration)
+    }
+    
+    public func courseButton(configuration: Any) -> AnyView? {
+        return _courseButton(configuration)
+    }
+}

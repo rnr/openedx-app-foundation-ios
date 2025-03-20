0e1b138d56b783cca81be7a5cf51fca163ccc4e3
Vadim Kuznetsov <vadim@touchappmedia.com>
2025-03-20 22:20:58 +0300

    chore: added protocols

--- Sources/OEXFoundation/IAP/IAPService.swift
+++ Sources/OEXFoundation/IAP/IAPService.swift
@@ -7,53 +7,68 @@
 
 import SwiftUI
 
-public protocol IAPConfiguration {
-    var productName: String { get }
-    var sku: String { get }
-    var courseID: String { get }
+/// A protocol that represents an in-app purchase product. Use to create views and request info about product.
+public protocol IAPProduct: Sendable {}
+
+/// A protocol that represents information about an in-app purchase product. Like sku or product id in store.
+public protocol IAPProductInfo: Sendable {}
+
+/// A struct that provides methods for managing in-app purchase products and their information.
+public struct IAPProductProvider<Product: IAPProduct, Info: IAPProductInfo> {
+    public typealias RequestInfoBlock = @Sendable (Product) async -> Info?
+    public typealias ProductBlock = @Sendable (Any) -> Product?
+    
+    private let requestBlock: RequestInfoBlock?
+    private let productBlock: ProductBlock
+
+    /// Initializes the provider with an async/await request block and a product block.
+    /// - Parameters:
+    ///   - request: An async/await function to request information for a product.
+    ///   - product: A function to convert an `Any` type into a `Product`.
+    public init(request: @escaping RequestInfoBlock, product: @escaping ProductBlock) {
+        requestBlock = nil
+        productBlock = product
+    }
+
+    /// Converts an `Any` type into a `Product` using the provided product block.
+    /// - Parameter object: The object to convert.
+    /// - Returns: A `Product` instance.
+    public func product(for object: Any) -> Product? {
+        productBlock(object)
+    }
+
+    /// An async/await method to request information for a given product.
+    /// - Parameter product: The product to request information for.
+    /// - Returns: The requested `Info`.
+    public func requestInfo(for product: Product) async -> Info? {
+        if let requestBlock {
+            return await requestBlock(product)
+        }
+        return nil
+    }
 }
 
+/// A protocol that defines the requirements for an in-app purchase service.
+/// Conforming types must operate on a specific type of `IAPProduct` and `IAPProductInfo`.
 @MainActor
 public protocol IAPServiceProtocol {
-    associatedtype Configuration: IAPConfiguration
-    func dashboardPrimaryCardButton(configuration: Configuration) -> AnyView
-    func courseButton(configuration: Configuration) -> AnyView
-}
-
-@MainActor
-public protocol AnyIAPServiceProtocol {
-    init<Service: IAPServiceProtocol>(_ service: Service)
+    associatedtype Product: IAPProduct
+    associatedtype ProductInfo: IAPProductInfo
     
-    func dashboardPrimaryCardButton(configuration: Any) -> AnyView?
-    func courseButton(configuration: Any) -> AnyView?
-}
-
-@MainActor
-public struct AnyIAPService: AnyIAPServiceProtocol {
-    private let _dashboardPrimaryCardButton: (Any) -> AnyView?
-    private let _courseButton: (Any) -> AnyView?
-
-    public init<Service: IAPServiceProtocol>(_ service: Service) {
-        _dashboardPrimaryCardButton = { config in
-            guard let config = config as? Service.Configuration else {
-                return nil
-            }
-            return service.dashboardPrimaryCardButton(configuration: config)
-        }
-        
-        _courseButton = { config in
-            guard let config = config as? Service.Configuration else {
-                return nil
-            }
-            return service.courseButton(configuration: config)
-        }
-    }
+    /// The provider used to manage product and product information requests.
+    var provider: IAPProductProvider<Product, ProductInfo> { get }
     
-    public func dashboardPrimaryCardButton(configuration: Any) -> AnyView? {
-        return _dashboardPrimaryCardButton(configuration)
-    }
+    /// Converts an `Any` type into a `Product` using the provider's product block.
+    /// - Parameter object: The object to convert.
+    /// - Returns: A `Product` instance.
+    func product(for object: Any) -> Product?
     
-    public func courseButton(configuration: Any) -> AnyView? {
-        return _courseButton(configuration)
-    }
+    /// Initiates a purchase for the specified product.
+    /// - Parameter product: The product to purchase.
+    func buy(product: Product) // Will return payment result.
+    
+    /// Returns a SwiftUI view for a given object.
+    /// - Parameter object: The object to create a view for.
+    /// - Returns: An `AnyView` instance representing the view for the object.
+    func view(for object: Any) -> AnyView?
 }
